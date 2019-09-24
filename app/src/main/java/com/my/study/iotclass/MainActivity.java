b8d04a6e798a7ed8f3db7f0e9956a5498185ca93
moon <1160045672@qq.com>
2019-09-24 22:08:44 +0800

    所有页面内嵌尝试

--- app/src/main/java/com/my/study/iotclass/MainActivity.java
+++ app/src/main/java/com/my/study/iotclass/MainActivity.java
@@ -5,12 +5,16 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
+    private WebView mWebView;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -39,6 +43,22 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mWebView = (WebView) findViewById(R.id.test_web_view);
+//解决点击链接跳转浏览器问题
+        mWebView.setWebViewClient(new WebViewClient());
+//js支持
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+//允许访问assets目录
+        settings.setAllowFileAccess(true);
+//设置WebView排版算法, 实现单列显示, 不允许横向移动
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+//assets文件路径
+        String path = "file:///android_asset/home.html";
+
+//加载Html页面
+        mWebView.loadUrl(path);
     }
 
 }
